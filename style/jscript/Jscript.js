ce923ed408668373022a6743223921606da7ffae
Vova-Pechenyi <vovaprostvova@gmail.com>
2017-05-03 19:49:06 +0400

    services

--- style/jscript/Jscript.js
+++ style/jscript/Jscript.js
@@ -8,28 +8,5 @@ $(function() {
         autoplaySpeed: 3000,
         slidesToShow: 3,
     });
-    
-    $('.featured-images').slick({
-        autoplay: true,
-        autoplaySpeed: 000,
-        speed: 5000,
-        cssEase: 'linear',
-        slidesToShow: 2,
-    });
-    
-    $('.clients-slider').slick({
-        slidesToShow: 2,
-        dots: true,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        speed: 1000,
-    });
-    $('.bgr-brands').slick({
-        slidesToShow: 5,
-        autoplay: true,
-        autoplaySpeed: 000,
-        speed: 3000,
-        cssEase: 'linear',
-    });
+    new WOW().init();
 })
