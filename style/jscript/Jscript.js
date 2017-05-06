71020cc4a0717fa3a12b8aa9b658ca2d684b9cbf
Vova-Pechenyi <vovaprostvova@gmail.com>
2017-05-06 22:02:47 +0400

    ready

--- style/jscript/Jscript.js
+++ style/jscript/Jscript.js
@@ -8,5 +8,21 @@ $(function() {
         autoplaySpeed: 3000,
         slidesToShow: 3,
     });
+    
+    
+    $('.slider-clients').slick({
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 6800,
+        speed: 200,
+        cssEase: "linear",
+        pauseOnHover: false,
+        arrows: false,
+    });
+    $('.menu-bars').click(function () {
+        $('.menu-bars').children('ul').slideToggle('500');
+    });
     new WOW().init();
 })
+
+
